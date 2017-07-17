657bcacbb52ac819669b5f64f797cd4b392c65b1
diamant1991 <diamant1988@mail.ru>
2017-07-17 09:24:23 +0300

    add files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,115 @@
-$(".product-carousel").owlCarousel({loop:!1,margin:20,nav:!0,dots:!1,responsive:{0:{items:1},550:{items:2}}}),$(".news-carousel").owlCarousel({loop:!1,margin:50,nav:!0,dots:!1,responsive:{0:{items:1},450:{items:2},767:{items:3}}}),$(".review-carousel").owlCarousel({loop:!1,margin:20,nav:!0,dots:!1,responsive:{0:{items:1},550:{items:2}}}),$("#card-slider").bxSlider({pagerCustom:"#thumb",mode:"fade"}),window.matchMedia("(max-width: 767px)").matches&&$(".catalog-menu__title").addClass("dropdown-btn"),$(".dropdown-btn").click(function(s){s.preventDefault();var e=$(this).next();e.is(":hidden")?($(this).addClass("active"),e.slideDown(250)):($(this).removeClass("active"),e.slideUp(250))});var review_stars=$(".j-rating a");$(review_stars).hover(function(){$(this).parent().find(review_stars).removeClass("is-hover is-active m-select m-active"),$(this).addClass("is-hover");var s=$(this).parent().find(review_stars);$(s).each(function(){if($(this).addClass("is-active"),$(this).hasClass("is-hover"))return!1})},function(){var s=$(this).parent().find("input[type=hidden]").val();$(this).parent().find(review_stars).removeClass("is-hover is-active m-select m-active");var e=$(this).parent().find(review_stars);s>=1&&$(e).each(function(){if($(this).addClass("m-select"),$(this).data("index")==s)return $(this).addClass("m-active"),!1})}),$(review_stars).click(function(){$(this).addClass("m-active");var s=$(this).parent().find(review_stars);return $(s).each(function(){if($(this).addClass("m-select"),$(this).hasClass("m-active"))return!1}),$(this).parent().find("input[type=hidden]").val(parseInt($(this).data("index"))),!1});
+$('.product-carousel').owlCarousel({
+  loop:false,
+  margin:20,
+  nav:true,
+  dots: false,
+  responsive:{
+    0:{
+        items:1
+    },
+    550:{
+        items:2
+    }
+  }
+})
+$('.news-carousel').owlCarousel({
+  loop:false,
+  margin:50,
+  nav:true,
+  dots: false,
+  responsive:{
+    0:{
+        items:1
+    },
+    450:{
+        items:2
+    },
+    767:{
+        items:3
+    }
+  }
+})
+$('.review-carousel').owlCarousel({
+  loop:false,
+  margin:20,
+  nav:true,
+  dots: false,
+  responsive:{
+    0:{
+        items:1
+    },
+    550:{
+        items:2
+    }
+  }
+})
+
+$('#card-slider').bxSlider({
+  pagerCustom: '#thumb',
+  mode: 'fade'
+});
+
+
+if(window.matchMedia('(max-width: 767px)').matches){
+  $('.catalog-menu__title').addClass('dropdown-btn')
+}
+$('.catalog-btn').click(function() {
+  var catalog = $('#catalog-list')
+  if(catalog.is(":hidden")){
+    $(this).addClass('active')
+    catalog.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    catalog.slideUp(250)
+  }
+});
+$('.dropdown-btn').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(":hidden")){
+    $(this).addClass('active')
+    menu.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    menu.slideUp(250)
+  }
+});
+  var review_stars = $('.j-rating a');
+    $(review_stars).hover(function () {
+        $(this).parent().find(review_stars).removeClass('is-hover is-active m-select m-active');
+        $(this).addClass('is-hover');
+        var thisStar = $(this).parent().find(review_stars);
+        $(thisStar).each(function () {
+            $(this).addClass('is-active');
+            if ( $(this).hasClass('is-hover') ) {
+                return false;
+            }
+        });
+    }, function () {
+        var value = $(this).parent().find('input[type=hidden]').val();
+        $(this).parent().find(review_stars).removeClass('is-hover is-active m-select m-active');
+        var thisStar = $(this).parent().find(review_stars);
+        if( value >= 1 ) {
+            $(thisStar).each(function () {
+                $(this).addClass('m-select');
+                if ($(this).data('index') == value) {
+                    $(this).addClass('m-active');
+                    return false;
+                }
+            });
+        }
+    });
+    $(review_stars).click(function () {
+        $(this).addClass('m-active');
+        var thisStar = $(this).parent().find(review_stars);
+        $(thisStar).each(function () {
+            $(this).addClass('m-select');
+            if ( $(this).hasClass('m-active') ) {
+                return false;
+            }
+        });
+        $(this).parent().find('input[type=hidden]').val(parseInt($(this).data('index')));
+        return false;
+    });
